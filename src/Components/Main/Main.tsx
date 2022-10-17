99c91c7622cfde816f8165fddc571372ada8f1fe
Shaha-Alam <shahaalam3002@gmail.com>
2022-10-17 14:23:58 +0600

    skills add

--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -40,7 +40,129 @@ export default function Main() {
             })}
           </div>
 
+          <div className={toggleState === 2 ? 'skills__content grid filters__active' : 'skills__content grid'}>
+            <div className="skills__area">
+              <h3 className="skills__title">Frontend Developer</h3>
 
+              <div className="skills__box">
+                <div className="skills__group">
+                  <div className="skills__data">
+                    <i className="ri-checkbox-circle-line"></i>
+                    <div>
+                      <h3 className="skills__name">HTML</h3>
+                      <span className="skills__level">Basic</span>
+                    </div>
+                  </div>
+
+
+                  <div className="skills__data">
+                    <i className="ri-checkbox-circle-line"></i>
+                    <div>
+                      <h3 className="skills__name">CSS</h3>
+                      <span className="skills__level">Advanced</span>
+                    </div>
+                  </div>
+
+
+                  <div className="skills__data">
+                    <i className="ri-checkbox-circle-line"></i>
+                    <div>
+                      <h3 className="skills__name">JavaScript</h3>
+                      <span className="skills__level">Intermediate</span>
+                    </div>
+                  </div>
+                </div>
+                
+                <div className="skills__group">
+                  <div className="skills__data">
+                    <i className="ri-checkbox-circle-line"></i>
+                    <div>
+                      <h3 className="skills__name">React</h3>
+                      <span className="skills__level">Intermediate</span>
+                    </div>
+                  </div>
+
+
+                  <div className="skills__data">
+                    <i className="ri-checkbox-circle-line"></i>
+                    <div>
+                      <h3 className="skills__name">Bootstrap</h3>
+                      <span className="skills__level">Intermediate</span>
+                    </div>
+                  </div>
+
+
+                  <div className="skills__data">
+                    <i className="ri-checkbox-circle-line"></i>
+                    <div>
+                      <h3 className="skills__name">Git</h3>
+                      <span className="skills__level">Intermediate</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="skills__area">
+              <h3 className="skills__title">Backend Developer</h3>
+
+              <div className="skills__box">
+                <div className="skills__group">
+
+                  <div className="skills__data">
+                    <i className="ri-checkbox-circle-line"></i>
+                    <div>
+                      <h3 className="skills__name">PHP</h3>
+                      <span className="skills__level">Intermediate</span>
+                    </div>
+                  </div>
+
+                  <div className="skills__data">
+                    <i className="ri-checkbox-circle-line"></i>
+                    <div>
+                      <h3 className="skills__name">MySQL</h3>
+                      <span className="skills__level">Advanced</span>
+                    </div>
+                  </div>
+
+                  <div className="skills__data">
+                    <i className="ri-checkbox-circle-line"></i>
+                    <div>
+                      <h3 className="skills__name">Firebase</h3>
+                      <span className="skills__level">Intermediate</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="skills__group">
+                  <div className="skills__data">
+                    <i className="ri-checkbox-circle-line"></i>
+                    <div>
+                      <h3 className="skills__name">Python</h3>
+                      <span className="skills__level">Basic</span>
+                    </div>
+                  </div>
+
+
+                  <div className="skills__data">
+                    <i className="ri-checkbox-circle-line"></i>
+                    <div>
+                      <h3 className="skills__name">Node Js</h3>
+                      <span className="skills__level">Intermediate</span>
+                    </div>
+                  </div>
+
+                  <div className="skills__data">
+                    <i className="ri-checkbox-circle-line"></i>
+                    <div>
+                      <h3 className="skills__name">Express JS</h3>
+                      <span className="skills__level">Intermediate</span>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </div>
 
         </div>
       </section>
