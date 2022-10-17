7d48ff366cb86c7a7dbe55b84285d501c6038dd9
Shaha-Alam <shahaalam3002@gmail.com>
2022-10-17 20:38:31 +0600

    data link

--- src/Components/Main/Data.tsx
+++ src/Components/Main/Data.tsx
@@ -1,43 +1,35 @@
 import project1 from '../../Assets/project1.jpg'
 import project2 from '../../Assets/project2.jpg'
 import project3 from '../../Assets/project3.jpg'
-//import project4 from '../../Assets/project4.jpg'
-//import project5 from '../../Assets/project5.jpg'
+import project4 from '../../Assets/project4.jpg'
 
 export const projectsData = [
     {
         id: 1,
         image: project1,
-        title: "Payment Site",
-        subtitle: "Web",
-        url: "/"
+        title: "Product Analysis",
+        
+        url: "https://09-assignment-product-analysis-website-shaha-alam.netlify.app/"
     },
     {
         id: 2,
         image: project2,
         title: "Portfolio Site",
         subtitle: "Web",
-        url: "/"
+        url: ""
     },
     {
         id: 3,
         image: project3,
-        title: "Fast Food App",
+        title: "Insta-Shohor",
         subtitle: "Mobile",
-        url: "/"
+        url: "https://shaha-alam.github.io/07_Explore-Brower-Debug_Insta-Shohor_Shaha-Alam/"
     },
-    /*{
+    {
         id: 4,
         image: project4,
         title: "Travel App",
-        subtitle: "Mobile",
-        url: "/"
+        url: "https://shaha-alam.github.io/05_DOM-JS--Balance-Checker_shaha-alam/"
     },
-    {
-        id: 5,
-        image: project5,
-        title: "Music App design",
-        subtitle: "Design",
-        url: "/"
-    }*/
+    
 ]
